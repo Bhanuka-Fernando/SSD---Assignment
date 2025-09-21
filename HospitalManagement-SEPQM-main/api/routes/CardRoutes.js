6ed80da43be36376b1ddb1e196eae40c199ac550
Sachintha Waduge <isurusachin123@gmail.com>
2025-09-22 00:30:41 +0530

    delete files

--- HospitalManagement-SEPQM-main/api/routes/CardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cardController = require('../Controllers/controller.card');
-
-// POST request to add a new card
-router.post('/', cardController.addCard);
-
-module.exports = router;
